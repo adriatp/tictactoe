c755d30bce6dbd2054219569d0288bfc6bfa7a0b
adriatp <tarradas.adria@gmail.com>
2023-09-05 21:01:30 +0200

    Fix win methods & add empty_cells logic

--- src/board.js
+++ src/board.js
@@ -5,21 +5,24 @@ class Board {
     for (let i = this.size; i--;) {
       this.matrix.push(new Array(this.size).fill(null));
     }
-    this.clear_cells = [...Array(this.size).keys()];
+  }
+
+  finished () {
+    return this.win() || this.full();
+  }
+
+  full () {
+    return this.empty_cells().length == 0;
   }
 
   win () {
-    if (this.row_same_color() || this.col_same_color() || this.diag_same_color() || this.cdiag_same_color()) {
-      return true;
-    } else {
-      return false;
-    }
+    return (this.row_same_color() || this.col_same_color() || this.diag_same_color() || this.cdiag_same_color());
   }
 
   row_same_color () {
     for (let i = 0; i < this.size; i++) {
       const firstColor = this.matrix[i][0];
-      if (firstColor === null) {
+      if (firstColor !== null) {
         let j = 0;
         while (j < this.size && firstColor === this.matrix[i][j]) {
           j++;
@@ -35,7 +38,7 @@ class Board {
   col_same_color () {
     for (let j = 0; j < this.size; j++) {
       const firstColor = this.matrix[0][j];
-      if (firstColor === null) {
+      if (firstColor !== null) {
         let i = 0;
         while (i < this.size && firstColor === this.matrix[i][j]) {
           i++;
@@ -50,6 +53,8 @@ class Board {
 
   diag_same_color () {
     const firstColor = this.matrix[0][0];
+    if (firstColor === null)
+      return false;
     for (let k = 0; k < this.size; k++) {
       if (firstColor !== this.matrix[k][k]) {
         return false;
@@ -59,16 +64,30 @@ class Board {
   }
 
   cdiag_same_color () {
-    const firstColor = this.matrix[0][0];
-    let i = 1;
-    let j = this.size - 2;
-    while (i < this.size()) {
+    let i = 0;
+    let j = this.size-1;
+    const firstColor = this.matrix[i][j];
+    if (firstColor === null)
+      return false;
+    while (i < this.size) {
       if (firstColor !== this.matrix[i][j]) {
         return false;
       }
       i++; j--;
     }
     return true;
+  }
+
+  empty_cells() {
+    let empty_cells = [];
+    for (let i=0; i<this.size; i++) {
+      for (let j=0; j<this.size; j++) {
+        if (this.matrix[i][j] === null) {
+          empty_cells.push([i,j]);
+        }
+      }
+    }
+    return empty_cells;
   }
 
   print () {
