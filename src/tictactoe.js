0780ec24521ceaf7ad3d723c79f01e0bf2457e18
adriatp <tarradas.adria@gmail.com>
2023-09-05 21:02:05 +0200

    Run method & add player logic

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,13 +1,27 @@
 const { Board } = require('./board.js');
-
+const { Player } = require('./player.js');
 class TicTacToe {
   constructor () {
     this.size = 3;
+    this.players = [ new Player('X', 'asdf'), new Player('O') ]
     this.board = new Board(this.size);
+    this.turn = 0;
+    this.current_player_position = 0;
   }
 
   run () {
-    this.board.print();
+    while(!this.board.finished()) {
+      let current_player = this.players[this.current_player_position];
+      current_player.move(this.board);
+      this.board.print();
+      console.log('\n');
+      if (!this.board.finished()) {
+        this.current_player_position++;
+        this.turn++;
+        if (this.current_player_position === this.players.length)
+          this.current_player_position = 0;
+      }
+    }
   }
 }
 
