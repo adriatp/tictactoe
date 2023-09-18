477e5cd1834a4027baa85fae8b60d0988b785c61
adriatp <tarradas.adria@gmail.com>
2023-09-18 19:11:05 +0200

    todo: refractoring minimax_move

--- src/player.js
+++ src/player.js
@@ -10,41 +10,52 @@ class Player {
 
   move (board, players) {
     if (this.algMove === 'minimax') {
-      const valMove = this.minimax_move(board, players, this.position);
-      console.log(valMove[0]);
-      board.matrix[valMove[1][0]][valMove[1][1]] = this.piece;
+      const valMove = this.minimax_move(board, players);
+      board.matrix[valMove[0]][valMove[1]] = this.piece;
     } else if (this.algMove === 'keyboard') {
       const valMove = this.keyboard_move(board);
-      board.matrix[valMove[1][0]][valMove[1][1]] = this.piece;
+      board.matrix[valMove[0]][valMove[1]] = this.piece;
     } else {
       const valMove = this.random_move(board);
-      board.matrix[valMove[1][0]][valMove[1][1]] = this.piece;
+      board.matrix[valMove[0]][valMove[1]] = this.piece;
     }
   }
 
-  minimax_move (board, players, originalPosition) {
-    const nextPlayer = this.next_player(players);
+  minimax_move (board, players) {
     const emptyCells = board.empty_cells();
-    let maxVal = null;
-    let maxCell = null;
-    let val = null;
+    const minimaxValues = [];
     for (let i = 0; i < emptyCells.length; i++) {
       board.matrix[emptyCells[i][0]][emptyCells[i][1]] = this.piece;
-      if (board.win()) {
-        maxVal = this.position === originalPosition ? 1 : -1;
-        maxCell = emptyCells[i];
-      } else if (board.full()) {
-        maxVal = 0;
-        maxCell = emptyCells[i];
-      } else {
-        val = nextPlayer.minimax_move(board, players, this.position);
-        maxVal = val[0];
-        maxCell = emptyCells[i];
-      }
+      const minimaxVal = this.minimax_move(board, players, this.position);
+      minimaxValues.push(minimaxVal);
       board.matrix[emptyCells[i][0]][emptyCells[i][1]] = null;
-      if (maxVal[0] === 1 || maxVal[0] === -1) break;
     }
-    return [maxVal, maxCell];
+    const maxMinimaxIdx = minimaxValues.indexOf(Math.max(...minimaxValues));
+
+    return emptyCells[maxMinimaxIdx];
+  }
+
+  minimax_move_rec (board, players, originalPosition) {
+    const emptyCells = board.empty_cells();
+    const nextPlayer = this.next_player(players);
+    const winningMoves = board.winning_moves(this.piece);
+    if (winningMoves.length > 0) {
+      return this.position === originalPosition ? 1 : -1;
+    } else if (emptyCells.length === 1) {
+      return emptyCells[0];
+    } else {
+      let i = 0;
+      const minimaxValues = [];
+      while (i < emptyCells.length) {
+        board.matrix[emptyCells[i][0]][emptyCells[i][1]] = this.piece;
+        const minimaxVal = nextPlayer.minimax_move(board, players, originalPosition);
+        if (this.minmaxVal !== 0) return minimaxVal;
+        minimaxValues.push(minimaxVal);
+        board.matrix[emptyCells[i][0]][emptyCells[i][1]] = null;
+        i++;
+      }
+      return 0;
+    }
   }
 
   keyboard_move (board) {
@@ -67,14 +78,14 @@ class Player {
         console.log('Invalid option, choose a correct movement:');
       }
     } while (selectedOption === null);
-    return [0, selectedOption];
+    return selectedOption;
   }
 
   random_move (board) {
     const emptyCells = board.empty_cells();
     const randomEmptyCellPos = Math.floor(Math.random() * emptyCells.length);
     const randomEmptyCell = emptyCells[randomEmptyCellPos];
-    return [0, randomEmptyCell];
+    return randomEmptyCell;
   }
 
   next_player (players) {
